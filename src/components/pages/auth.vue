994631861f2764c326187cb9f12b93047cd6b1ad
Roman <roman@MacBook-Pro-Roman.local>
2024-06-14 12:34:34 +0300

    first commit

---
+++ src/components/pages/auth.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="auth">
+    <div class="auth-wrap">
+      <div class="auth-column">
+        <div class="auth-title">Вход</div>
+
+        
+      </div>
+      <div class="auth-column"></div>
+    </div>
+  </div>
+</template>
